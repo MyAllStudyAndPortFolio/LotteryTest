ab032f6a78c4e692b407a20586f5a76216f93d1a
HalfWing_lab <halfwing1998@gmail.com>
2022-01-07 18:02:59 +0900

    finish making ui execption

--- app/src/main/java/com/example/lotterypart/MainActivity.kt
+++ app/src/main/java/com/example/lotterypart/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.lotterypart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.NumberPicker
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +25,10 @@ class MainActivity : AppCompatActivity() {
         findViewById<NumberPicker>(R.id.numberPicker)
     }
 
+    private var didRun = false
+
+    private var pickNumberSet = hashSetOf<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -31,11 +37,47 @@ class MainActivity : AppCompatActivity() {
         numberPicker.maxValue = 45
 
         initRunButton()
+        initAddButton()
     }
 
     private fun initRunButton(){
         runButton.setOnClickListener {
             val list = getRandomNumber()
+
+            Log.d("MainActivity",list.toString())
         }
+    }
+    private fun initAddButton(){
+        addButton.setOnClickListener {
+            if(didRun){
+                Toast.makeText(this, "초기화 후 다시 시도 바랍니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if(pickNumberSet.size>=5){
+                Toast.makeText(this, "번호는 최대 5개 입니다.",Toast.LENGTH_SHORT).show()
+
+            }
+            if(pickNumberSet.contains(numberPicker.value)){
+                Toast.makeText(this, "이미 선택하신 번호 입니다 다시 입력해 주세요", Toast.LENGTH_SHORT).show()
+            }
+            
+        }
+        
+        
+    }
+
+    private fun getRandomNumber() : List<Int>{
+        val numberList = mutableListOf<Int>()
+            .apply {
+                for (i in 1..45){
+                    this.add(i)
+                }
+            }
+
+        numberList.shuffle()
+        val newList = numberList.subList(0,6)
+        return newList.sorted()
+
+
     }
 }
