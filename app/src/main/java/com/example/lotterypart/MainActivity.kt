d14fd273b501460fe6391782638749e84f06f060
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2022-01-07 16:30:43 +0900

    almost ui finish

--- app/src/main/java/com/example/lotterypart/MainActivity.kt
+++ app/src/main/java/com/example/lotterypart/MainActivity.kt
@@ -2,8 +2,17 @@ package com.example.lotterypart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private val clearButton: Button by lazy {
+        findViewById<Button>(R.id.clearButton)
+    }
+    private val addButton: Button by lazy{
+        findViewById<Button>(R.id.addButton)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
