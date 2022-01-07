f101e04660374d42931129cc31b1e8d731829511
HalfWing_lab <halfwing1998@gmail.com>
2022-01-07 17:11:56 +0900

    megeing temp

--- app/src/main/java/com/example/lotterypart/MainActivity.kt
+++ app/src/main/java/com/example/lotterypart/MainActivity.kt
@@ -3,18 +3,39 @@ package com.example.lotterypart
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.NumberPicker
 
 class MainActivity : AppCompatActivity() {
 
-    private val clearButton: Button by lazy {
+    private val clearButton: Button by lazy{
         findViewById<Button>(R.id.clearButton)
     }
+
     private val addButton: Button by lazy{
         findViewById<Button>(R.id.addButton)
+    }
+
+    private val runButton: Button by lazy{
+        findViewById<Button>(R.id.runButton)
+    }
+
+    private val numberPicker: NumberPicker by lazy{
+        findViewById<NumberPicker>(R.id.numberPicker)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        numberPicker.minValue = 1
+        numberPicker.maxValue = 45
+
+        initRunButton()
+    }
+
+    private fun initRunButton(){
+        runButton.setOnClickListener {
+            val list = getRandomNumber()
+        }
     }
 }
